7ed8e22eed0ad8317e56fa89fa95663feb5a06a5
rajneeshmahala <rajneeshmahala@gmail.com>
2026-03-01 02:30:12 +0530

    automate ansible inventory

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,28 @@ output "vm2_private_ip" {
 output "vm3_private_ip" {
   value = azurerm_network_interface.nic3.private_ip_address
 }
+
+output "ansible_inventory" {
+  value = <<EOT
+[web]
+vm-web ansible_host=${azurerm_public_ip.vm1_pip.ip_address}
+
+[app]
+vm-app ansible_host=${azurerm_network_interface.nic2.private_ip_address}
+
+[db]
+vm-db ansible_host=${azurerm_network_interface.nic3.private_ip_address}
+
+[private:children]
+app
+db
+
+[all:vars]
+ansible_user=${var.admin_username}
+ansible_ssh_private_key_file=~/.ssh/id_rsa
+
+[private:vars]
+ansible_ssh_common_args='-o ProxyJump=${var.admin_username}@${azurerm_public_ip.vm1_pip.ip_address}'
+EOT
+}
+#terraform output -raw ansible_inventory > ../ansible_vm_setup/inventory.ini
