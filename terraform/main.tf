c51ae604dedbec4ae0d04d16ca41cfcc81da4efb
Rajneesh <40818730+rajneeshmahala@users.noreply.github.com>
2026-03-18 13:37:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -125,58 +125,58 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm2" {
-  name                            = "vm2-private"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = var.location
-  size                            = var.vm_size
-  admin_username                  = var.admin_username
-  disable_password_authentication = true
+#resource "azurerm_linux_virtual_machine" "vm2" {
+#  name                            = "vm2-private"
+#  resource_group_name             = azurerm_resource_group.rg.name
+#  location                        = var.location
+#  size                            = var.vm_size
+#  admin_username                  = var.admin_username
+#  disable_password_authentication = true
+#
+#  admin_ssh_key {
+#    username   = var.admin_username
+#    public_key = var.admin_ssh_public_key
+#  }
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_public_key
-  }
+#  network_interface_ids = [azurerm_network_interface.nic2.id]
 
-  network_interface_ids = [azurerm_network_interface.nic2.id]
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#  source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "0001-com-ubuntu-server-jammy"
+#    sku       = "22_04-lts-gen2"
+#    version   = "latest"
+#  }
+#}
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-}
+#resource "azurerm_linux_virtual_machine" "vm3" {
+#  name                            = "vm3-private"
+#  resource_group_name             = azurerm_resource_group.rg.name
+#  location                        = var.location
+#  size                            = var.vm_size
+#  admin_username                  = var.admin_username
+#  disable_password_authentication = true
 
-resource "azurerm_linux_virtual_machine" "vm3" {
-  name                            = "vm3-private"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = var.location
-  size                            = var.vm_size
-  admin_username                  = var.admin_username
-  disable_password_authentication = true
+#  admin_ssh_key {
+#    username   = var.admin_username
+#    public_key = var.admin_ssh_public_key
+#  }
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_public_key
-  }
+#  network_interface_ids = [azurerm_network_interface.nic3.id]
 
-  network_interface_ids = [azurerm_network_interface.nic3.id]
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-}
+# source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "0001-com-ubuntu-server-jammy"
+#    sku       = "22_04-lts-gen2"
+#    version   = "latest"
+#  }
+#}
